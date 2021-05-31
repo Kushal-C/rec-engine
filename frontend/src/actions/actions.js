dafd482f5e0fb267bb15398a07aabbde0341f159
Kushal-C <kcutari@gmail.com>
2021-05-31 12:32:59 -0700

    Moved landing images to function

---
+++ frontend/src/actions/actions.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import image_1 from '../images/image_1.jpg';
+import image_2 from '../images/image_2.jpg';
+import image_3 from '../images/image_3.jpg';
+
+export function fetchLandingImages() {
+    return [image_1, image_2, image_3];
+};
+
+let facebookLogin = function() {
+
+}
+
+let googleLogin = function() {
+
+}
+
+//Retrieves - 15-20 products based on user settings
+let fetchProducts = function(userId) {
+
+}
--- frontend/src/components/landing.jsx
+++ frontend/src/components/landing.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import image_1 from '../images/image_1.jpg';
-import image_2 from '../images/image_2.jpg';
-import image_3 from '../images/image_3.jpg';
+import {fetchLandingImages} from '../actions/actions';
 
 function Landing() {
+
+    const images = fetchLandingImages().map((img, key) => {
+        return <li key={key} className="card"><img src={img} alt="fashion article"/></li>
+    });
+
     return (
         <div className="container">
             <div className="container__column container__landing">
@@ -12,14 +15,12 @@ function Landing() {
                 <h5 className="header header__landing"><span className="highlight">AI Fashion Done Right.</span></h5>
                 <p className="text__landing">Generate 1000’s of recommendations based off of your own personal tastes and outfits your already rock.</p>
         
-                <button id="oauth-btn" className="btn btn__landing" style={{"backgroundColor": "#FF6B00"}}> Signup with Google</button>
-                <button className="btn btn__landing" style={{"backgroundColor": "#0085FF"}}> Signup with Facebook</button>
+                <button id="oauth-btn" className="btn btn__landing btn__landing__google"> Signup with Google</button>
+                <button className="btn btn__landing btn__landing__facebook"> Signup with Facebook</button>
             </div>
             <div className="container__column container__landing card_container__landing">
                 <ul>
-                    <li className="card"><img src={image_3} alt="shoe"/></li>
-                    <li className="card"><img src={image_2} alt="shoe"/></li>
-                    <li className="card"><img src={image_1} alt="shoe"/></li>
+                    {images}
                 </ul>
             </div>
             
